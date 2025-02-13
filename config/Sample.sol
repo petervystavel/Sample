fc487e50bddff3d7e26aaab9bb20f79adc40f403
petervystavel <146931482+petervystavel@users.noreply.github.com>
2025-02-13 15:35:10 +0100

    fix

---
+++ config/Sample.sol
@@ -0,0 +1,10 @@
+{
+    "format_version": "12.00",
+    "minimum_version": "10.0.40219.1",
+    "projects": [
+        "Sample.prj"
+    ],
+    "solution_name": "Sample",
+    "version": "16",
+    "version_full": "16.0.30319.14"
+}
